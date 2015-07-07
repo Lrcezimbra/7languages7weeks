5ad9d2644e7c15f6dab83ded8fd1f8f89208ce5a
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2015-07-07 01:15:39 -0300

    Ruby day 2: Add read file exercise

---
+++ ruby/day2/read_file.rb
@@ -0,0 +1,12 @@
+class ReadFile
+  def read
+    regex = /neque eget iaculis tincidunt/
+    file = File.new('file.txt')
+    file.each do |line|
+      puts "Line #{file.lineno}: #{line}" if line[regex]
+    end
+  end
+end
+
+read_file = ReadFile.new
+read_file.read
