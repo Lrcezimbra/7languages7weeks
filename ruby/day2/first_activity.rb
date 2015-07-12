3a6079d63b1531f67c6f209dc02b8a943543fe00
Lucas Rangel Cezimbra <lucascezimbra@gmail.com>
2015-07-12 18:19:38 -0300

    Ruby: Merge classes and rspec

--- ruby/day2/first_activity.rb
+++ ruby/day2/first_activity.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 class FirstActivity
   def array_with_each
     array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
@@ -28,5 +30,21 @@ class FirstActivity
     end
 
     result
+  end
+end
+
+
+##############################################################
+
+
+RSpec.describe FirstActivity do
+  before { @firstActivity = FirstActivity.new }
+
+  it 'Using each, print the contents of an array of sixteen numbers, four numbers at a time.' do
+    expect(@firstActivity.array_with_each).to eq([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
+  end
+
+  it 'Using each_slice, print the contents of an array of sixteen numbers, four numbers at a time.' do
+    expect(@firstActivity.array_with_each_slice).to eq([[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]])
   end
 end
--- ruby/day2/tree.rb
+++ ruby/day2/tree.rb
@@ -1,3 +1,5 @@
+require 'rspec'
+
 class Tree 
   attr_accessor :childrens, :node_name
 
@@ -16,5 +18,45 @@ class Tree
   def visit_all(&block)
     visit &block
     childrens.each { |c| c.visit_all &block }
+  end
+end
+
+
+#############################################################
+
+
+RSpec.describe Tree do
+  context 'initialize with hash' do
+    before do
+      @tree = Tree.new ({
+        'grandpa' => {
+          'dad' => {
+            'child 1' => {},
+            'child 2' => {}
+          },
+          'uncle' => {
+            'child 3' => {},
+            'child 4' => {}
+          }
+        }
+      })
+    end
+
+    it 'save node_name' do
+      expect(@tree.node_name).to eq('grandpa')
+    end
+
+    it { expect(@tree.childrens.first.node_name).to eq('dad') }
+    it { expect(@tree.childrens.first.childrens.first.node_name).to eq('child 1') }
+    it { expect(@tree.childrens.first.childrens[1].node_name).to eq('child 2') }
+    it { expect(@tree.childrens[1].node_name).to eq('uncle') }
+    it { expect(@tree.childrens[1].childrens.first.node_name).to eq('child 3') }
+    it { expect(@tree.childrens[1].childrens[1].node_name).to eq('child 4') }
+
+    it 'should visit 7 nodes' do
+      count = 0
+      @tree.visit_all { |tree| count+=1 }
+      expect(count).to be(7)
+    end
   end
 end
